ba4a3c52ab08b5cdabc0ed5cabc38d400e5cc1bb
Aditya Mayukh Som <adi.kg2@gmail.com>
2022-05-20 18:35:26 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/App.css";
+import MediaPlayer from "./MediaPlayer";
+
+function App() {
+    return (
+        <div>
+            <MediaPlayer />
+        </div>
+    );
+}
+
+export default App;
